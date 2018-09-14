1899ab0b2a2d5c577e95f4befb3236ff6de8b12b
Caio Corrêa Chaves <caio.chaves@etec.sp.gov.br>
2018-09-14 17:49:18 -0300

    Paginas de login e de cadastro responsivas

--- views/usuario/login/view.php
+++ views/usuario/login/view.php
@@ -3,7 +3,7 @@
 <div class='row container'>
     <main class='col s12 l8 push-l2 white-text' style='background: rgba(49, 24, 0, 0.7);'>
         <div class='row'>
-            <section class='col s10 push-l1'>
+            <section class='col s10 push-s1'>
                 <h5 class='center' style='color: #FFF59D'> Entrar</h5>
                 <form class='row' name='login' method='POST' action='<?=INDEX?>/logar'> <!-- Correto -->
                     <div class='input-field col s12'>
@@ -14,6 +14,7 @@
                         <input type='password' name='senha' class='validate' required>
                         <label style='color: #FFF59D'>Senha</label>
                     </div>
+
                     <!-- Em caso de erro, ele será mostrado aqui -->
                     <?php if (!empty($_SESSION['error'])): ?>
                     <div class='red-text col s12' style="margin-bottom: 10px">
@@ -26,6 +27,8 @@
                         <?php unset($_SESSION['msg']); ?>
                     </div>
                     <?php endif ?>
+
+                    
                     <button class='btn col s6 push-s3 yellow lighten-3' style='color: #45260B'>
                         Logar
                     </button>
