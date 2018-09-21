979de1f528de1512701686bad16b9683f827f64a
Jess <jessicamour16@gmail.com>
2018-09-20 23:27:32 -0300

    Desenvolvimento da página de editar perfil

--- views/_includes/sections/edit_perfil.php
+++ views/_includes/sections/edit_perfil.php
@@ -1,7 +1,20 @@
 <?php if (!defined('RAIZ')) exit(); ?>
 
 <section title="Editar Perfil">
-	<div class="row" style="background: rgba(139,69,19,0.7);">
-		Div
+	<div class="row" style="background: rgba(139,69,19,0.7); margin-top: 1em;">
+		<form class="row" name="editar_perfil" method="POST"	action="<link do fomulario de acesso aó BD>">
+			<div class="input-field" style="margin-left: 3em;">
+                <label style='color: #FFF59D'>Nome</label><br>
+			</div>
+			<div class="input-field" style="margin-left: 3em;">
+				<label style='color: #FFF59D'>Sobrenome</label><br>
+            </div>
+            <div class="input-field" style="margin-left: 3em;">
+                <label style='color: #FFF59D'>E-mail</label><br>
+            </div>
+            <div class="input-field" style="margin-left: 3em;">
+                <label style='color: #FFF59D'>Senha</label><br>
+			</div>
+		</form>
 	</div>
 </section>
