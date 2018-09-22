e17ce0e0c04b037592079f8ee171ba5d31d91130
Caio Corrêa Chaves <caio.chaves@etec.sp.gov.br>
2018-09-21 23:03:25 -0300

    Conteúdo centralizado na página de edição de perfil

--- views/_includes/sections/edit_perfil.php
+++ views/_includes/sections/edit_perfil.php
@@ -1,32 +1,41 @@
 <?php if (!defined('RAIZ')) exit(); ?>
 
+<style type="text/css">
+    form .input-field label {
+        color: #FFF59D;
+        text-align: center !important;
+    }
+</style>
+
 <section title="Editar Perfil">
-	<div class="row col s4 center" style="background: rgba(139,69,19,0.7); margin-top: 1em; ">
-		<form class="row" name="editar_perfil" method="POST" action="<link do fomulario de acesso aó BD>">
-			<div class="input-field" style="margin-left: 3em;">
-                <label style='color: #FFF59D'>Nome</label>
-            </div>
-            <div class="input-field" style="margin-left: 9em;">
-                <label style='color: #FFF59D'>Editar</label><br>
-			</div>
-			<div class="input-field" style="margin-left: 3em;">
-				<label style='color: #FFF59D'>Sobrenome</label>
-            </div>
-            <div class="input-field" style="margin-left: 9em;">
-                <label style='color: #FFF59D'>Editar</label><br>
-			</div>
-            <div class="input-field" style="margin-left: 3em;">
-                <label style='color: #FFF59D'>E-mail</label>
-            </div>
-            <div class="input-field" style="margin-left: 9em;">
-                <label style='color: #FFF59D'>Editar</label><br>
-			</div>
-            <div class="input-field" style="margin-left: 3em;">
-                <label style='color: #FFF59D'>Senha</label>
-			</div>
-			<div class="input-field" style="margin-left: 9em;">
-                <label style='color: #FFF59D'>Editar</label><br>
-			</div>
-		</form>
+	<div class="row" style="background: rgba(139,69,19,0.7); margin-top: 1em;">
+        <div class="col l4 push-l4 red">
+    		<form class="row" name="editar_perfil" method="POST" action="<link do fomulario de acesso aó BD>">
+    			<div class="input-field col s9">
+                    <label>Nome</label>
+                </div>
+                <div class="input-field col s2">
+                    <label>Editar</label><br>
+    			</div>
+    			<div class="input-field col s9">
+    				<label>Sobrenome</label>
+                </div>
+                <div class="input-field col s2">
+                    <label>Editar</label><br>
+    			</div>
+                <div class="input-field col s9">
+                    <label>E-mail</label>
+                </div>
+                <div class="input-field col s2">
+                    <label>Editar</label><br>
+    			</div>
+                <div class="input-field col s9">
+                    <label>Senha</label>
+    			</div>
+    			<div class="input-field col s2">
+                    <label>Editar</label><br>
+    			</div>
+    		</form>
+        </div>
 	</div>
 </section>
