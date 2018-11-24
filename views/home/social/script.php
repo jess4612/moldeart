7c6a42ff779cc82d49af9ca4f414113dcb1a899a
Caio Corrêa Chaves <caio.chaves@etec.sp.gov.br>
2018-11-24 02:04:49 -0200

    Molde Social funcionando sem detalhamento na estilização

--- views/home/social/script.php
+++ views/home/social/script.php
@@ -1,9 +1,17 @@
 <?php
-if (!defined('RAIZ')) exit();
+use \Connection\Connection;
+
+if (!defined('RAIZ')) {
+    exit();
+}
 
 if (empty($_SESSION['userdata'])) {
     header('Location: ' . INDEX);
 }
+
+$con = new Connection('MoldeArt');
+$posts = $con->dbExec('SELECT * FROM E_Postagem');
+
 
 //Atribuir valores especificos
 $title = 'O que é MEI? - Molde Art';
--- views/home/social/view.php
+++ views/home/social/view.php
@@ -7,6 +7,26 @@
 </style>
 <main class="row container">
     <?php if ($_SESSION['userdata']->isAdmin()): ?>
-    <a class="btn molde-brown right" href="<?=INDEX?>/home/social/restrita">Area restrita</a>
+    <div class="row" style="margin-right: 0">
+        <a class="btn molde-brown right" href="<?=INDEX?>/home/social/restrita">Area restrita</a>
+    </div>
     <?php endif ?>
+
+    <h5>Bem vindo(a) ao Molde Social!!</h5>
+
+    <?php foreach ($posts as $key => $post): ?>
+    <div class="row">
+        <h5 class="col s12"><?=$post['POS_NOME']?> <span class="right"><?=implode('.', array_reverse(explode('-', $post['POS_DATA'])))?></span></h5>
+        <img src="<?=URL_UPLOAD?>/<?=$post['POS_CAPA']?>" class="col s12 m6 l3">
+        <p class="flow-text"><?=$post['POS_TEXTO']?></p>
+        <div class="carousel">
+            <?php foreach (unserialize($post['POS_IMAGENS']) as $key => $value): ?>
+
+            <a class="carousel-item" href="#!"><img src="<?=URL_IMG?>/thumb.php?src=<?=RAIZ?>/views/_upload/<?=$value?>&size=300x250"></a>
+
+            <?php endforeach ?>
+        </div>
+    </div>
+    <hr class="molde-brown-text">
+    <?php endforeach ?>
 </main>
